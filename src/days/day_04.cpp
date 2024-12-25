01532512f2bb6a43b46715b368a887a115d736c9
Bryan Castillo <castillo.bryan@gmail.com>
2024-12-24 18:14:42 -0800

    Doing day 4 part 2.

--- src/days/day_04.cpp
+++ src/days/day_04.cpp
@@ -1,6 +1,7 @@
 #include "day_04.hpp"
 
 #include <vector>
+#include <map>
 #include <iterator>
 #include <string>
 #include <variant>
@@ -12,56 +13,116 @@ namespace {
     using namespace rook::aoc;
     using namespace std;
 
-    static string TARGET_XMAS = string("XMAS");
+    static vector<tuple<int, int>> STANDARD_DIRS = {
+        {-1, -1},
+        {-1, 0},
+        {-1, 1},
+        {0, -1},
+        {0, 1},
+        {1, -1},
+        {1, 0},
+        {1, 1}
+    };
 
-    int countWithDirection(const vector<string>& lines, int x, int y, int dx, int dy) {
+    static vector<tuple<int, int>> DIAG_DIRS = {
+        {-1, -1},
+        {-1, 1},
+        {1, -1},
+        {1, 1},
+    };
+
+    bool existsAtWithDirection(const string& s, const vector<string>& lines, int x, int y, int dx, int dy) {
         if (lines.size() == 0) {
-            return 0;
+            return false;
         }
 
-        int width = (int) lines[0].size();
-        int height = (int) lines.size();
+        int width = (int)lines[0].size();
+        int height = (int)lines.size();
 
-        for (int i = 0; i < (int)TARGET_XMAS.size(); i++) {
+        if (x < 0 || x >= width || y < 0 || y >= height) {
+            return false;
+        }
+
+        for (int i = 0; i < (int)s.size(); i++) {
             int xp = x + i * dx;
             int yp = y + i * dy;
             if (xp < 0 || xp >= width || yp < 0 || yp >= height) {
-                return 0;
+                return false;
             }
-            else if (TARGET_XMAS[i] != lines[yp][xp]) {
-                return 0;
+            else if (s[i] != lines[yp][xp]) {
+                return false;
             }
         }
 
-        return 1;
+        return true;
     }
 
-    int countAtSpot(const vector<string>& lines, int x, int y) {
+    void getExistingDirections(string& tgt, const vector<tuple<int, int>>& possibleDirs,
+        const vector<string>& lines, int x, int y, vector<tuple<int, int>>& vecs)
+    {
         if (y < 0 || y >= lines.size() || x < 0 || x > lines[0].size()) {
-            return 0;
+            return;
         }
-        if (lines[y].at(x) != TARGET_XMAS.at(0)) {
-            return 0;
+        if (lines[y].at(x) != tgt.at(0)) {
+            return;
         }
-        return
-            countWithDirection(lines, x, y, -1, -1) +
-            countWithDirection(lines, x, y, -1, 0) +
-            countWithDirection(lines, x, y, -1, 1) +
-            countWithDirection(lines, x, y, 0, -1) +
-            countWithDirection(lines, x, y, 0, 1) +
-            countWithDirection(lines, x, y, 1, -1) +
-            countWithDirection(lines, x, y, 1, 0) +
-            countWithDirection(lines, x, y, 1, 1);
+
+        for (auto& dir : possibleDirs) {
+            if (existsAtWithDirection(tgt, lines, x, y, get<0>(dir), get<1>(dir))) {
+                vecs.push_back(dir);
+            }
+        }
     }
 
-    int getXMASCounts(const vector<string>& lines) {
+    int getXMASCountsPart1(const vector<string>& lines) {
         int width = (int) lines[0].size();
         int height = (int) lines.size();
 
+        string tgt("XMAS");
         int totalFound = 0;
+
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                totalFound += countAtSpot(lines, x, y);
+                vector<tuple<int, int>> dirs;
+                getExistingDirections(tgt, STANDARD_DIRS, lines, x, y, dirs);
+                totalFound += (int) dirs.size();
+            }
+        }
+
+        return totalFound;
+    }
+
+    int getXMASCountsPart2(const vector<string>& lines) {
+        int width = (int)lines[0].size();
+        int height = (int)lines.size();
+
+        string tgt("MAS");
+        map<tuple<int, int>, int> aSpots;
+        int totalFound = 0;
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                vector<tuple<int, int>> dirs;
+                getExistingDirections(tgt, DIAG_DIRS, lines, x, y, dirs);
+                for (auto& dir : dirs) {
+                    int ax = x + get<0>(dir);
+                    int ay = y + get<1>(dir);
+                    tuple<int, int> pos(ax, ay);
+
+                    auto it = aSpots.find(pos);
+                    if (it != aSpots.end()) {
+                        aSpots[pos] = it->second + 1;
+                    }
+                    else {
+                        aSpots[pos] = 1;
+                    }
+                }
+            }
+        }
+
+        for (auto& it : aSpots) {
+            if (it.second == 2) {
+                totalFound++;
             }
         }
 
@@ -75,13 +136,15 @@ namespace rook::aoc::days {
 
     string Day04::solvePart1(const string& input) {
         auto lines = ioReadLines(input);
-        int counts = getXMASCounts(lines);
+        int counts = getXMASCountsPart1(lines);
 
         return to_string(counts);
     }
 
     string Day04::solvePart2(const string& input) {
-        (void) input;
-        return "";
+        auto lines = ioReadLines(input);
+        int counts = getXMASCountsPart2(lines);
+
+        return to_string(counts);
     }
 }
